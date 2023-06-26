420101f233b775b75717182ff5be6efe83f776b7
AmanMulani <amanmulani369@gmail.com>
2023-06-26 19:57:24 +0530

    feat | AM | Odd-Even Program

---
+++ src/conditional/EvenOdd.java
@@ -0,0 +1,16 @@
+package src.conditional;
+
+public class EvenOdd {
+    
+    public static void main(String[] args) {
+
+        int number = 5;
+        
+        if(number % 2 == 0) {
+            System.out.println("The given number is even");
+        } else {
+            System.out.println("The given number is odd");
+        }
+        
+    }
+}
