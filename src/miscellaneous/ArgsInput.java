63281b2fcee98e692dc0591cf9c2002693c46dd9
AmanMulani <amanmulani369@gmail.com>
2023-07-05 19:50:20 +0530

    feat | AM | Input to main method

---
+++ src/miscellaneous/ArgsInput.java
@@ -0,0 +1,11 @@
+package src.miscellaneous;
+
+public class ArgsInput {
+    
+    public static void main(String[] args) {
+        
+        for(int i = 0; i<args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
