669c824c0bcf68f23180161b5187e8fd4c9310a4
AmanMulani <amanmulani369@gmail.com>
2023-07-18 21:08:55 +0530

    refactor | AM | Use functions

--- src/loops/PrimeNumber.java
+++ src/loops/PrimeNumber.java
@@ -5,21 +5,30 @@ public class PrimeNumber {
     //A Prime Number is a number which is only divisible by 1 and itself.
 
     public static void main(String[] args) {
-        int number = 169;
-        boolean isPrime = true;
-        
-        for(int i = 2; i<number/2; i++) {
-            if(number % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
+        int number = 17;
+        boolean isPrimeNumber = isPrime(number);
 
-        if(isPrime) {
+        if(isPrimeNumber) {
             System.out.println("Prime Number");
         } else {
             System.out.println("Not a prime number");
         }
 
+    }
+
+    public static boolean isPrime(int number) {
+        boolean isPrimeFlag = true;
+        for(int i = 2; i<number/2; i++) {
+            if(number % i == 0) {
+                isPrimeFlag = false;
+                break;
+            }
+        }
+
+        if(isPrimeFlag) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
